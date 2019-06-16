1321d718d96362448d80b21cb0ca440f2215dc76
ZhangKevin1 <977548213@qq.com>
2019-06-16 18:57:05 +0800

    增加任务列表及子任务列表分页功能

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -58,6 +58,7 @@ function  getSubTask(imageName,tid,task_name){
             $row.append("<td>" + (data[i]['task-status'] || "") + "</td>");
             $row.append("<td>" + (data[i]['priority'] || "") + "</td>");
         }
+        firstPage2();
     })
 }
 
@@ -145,8 +146,107 @@ function getTasks(imageName) {
                 openCard($("#subTaskCard"));
             });
         }
+        firstPage();
     })
 }
+
+//增加分页功能
+var pagesize = 10;   /*每页条数*/
+var currentPage;
+var startRow;
+var lastRow;
+function pagination(i){
+    departmentInfo = document.getElementById("taskTableBody");  /*获取table中的内容*/
+    totalRow = departmentInfo.rows.length;   /*计算出总条数(这种方法用来获取table行数，获取列数用var cells = departmentsInfo.rows[0].cells.length;*/
+    totalPage = Math.ceil(totalRow/pagesize);
+    currentPage = i;/*当前页*/
+    document.getElementById("numPage").value="第"+currentPage+"页";   /*显示页码*/
+    startRow = (currentPage-1)*pagesize;/*每页显示第一条数据的行数*/
+    lastRow = currentPage*pagesize;/*每页显示的最后一条数据的行数,因为表头也算一行，所以这里不需要减1，这种情况以自己的实际情况为准*/
+    if(lastRow>totalRow){
+        lastRow=totalRow;/*如果最后一页的最后一条数据显示的行数大于总条数，那么就让最后一条的行数等于总条数*/
+    }
+    for(var i = 0;i<totalRow;i++){   /*将数据遍历出来*/
+        var hrow = departmentInfo.rows[i];
+        if(i>=startRow&&i<lastRow){
+            hrow.style.display="table-row";
+        }else{
+            hrow.style.display="none";
+        }
+    }
+}
+function firstPage(){
+    var i = 1;
+    pagination(i);
+}
+function prevPage(){
+    var i= currentPage-1;
+    if(i<1){
+        i=currentPage;
+    }
+    pagination(i);
+}
+function pnextPage(){
+    var i= currentPage+1;
+    if(i>totalPage){
+        i= currentPage;
+    }
+    pagination(i);
+}
+function lastPage() {
+    var i = totalPage;
+    pagination(i);
+}
+
+var pagesize2 = 10;   /*每页条数*/
+var currentPage2;
+var startRow2;
+var lastRow2;
+function pagination2(i){
+    departmentInfo2 = document.getElementById("subTaskTableBody");  /*获取table中的内容*/
+    totalRow2 = departmentInfo2.rows.length;   /*计算出总条数(这种方法用来获取table行数，获取列数用var cells = departmentsInfo.rows[0].cells.length;*/
+    totalPage2 = Math.ceil(totalRow2/pagesize2);
+    currentPage2 = i;/*当前页*/
+    document.getElementById("numPage2").value="第"+currentPage2+"页";   /*显示页码*/
+    startRow2 = (currentPage2-1)*pagesize2;/*每页显示第一条数据的行数*/
+    lastRow2 = currentPage2*pagesize2;/*每页显示的最后一条数据的行数,因为表头也算一行，所以这里不需要减1，这种情况以自己的实际情况为准*/
+    if(lastRow2>totalRow2){
+        lastRow2=totalRow2;/*如果最后一页的最后一条数据显示的行数大于总条数，那么就让最后一条的行数等于总条数*/
+    }
+    for(var i = 0;i<totalRow2;i++){   /*将数据遍历出来*/
+        var hrow = departmentInfo2.rows[i];
+        if(i>=startRow2&&i<lastRow2){
+            hrow.style.display="table-row";
+        }else{
+            hrow.style.display="none";
+        }
+    }
+}
+function firstPage2(){
+    var i = 1;
+    pagination2(i);
+}
+function prevPage2(){
+    var i= currentPage2-1;
+    if(i<1){
+        i=currentPage2;
+    }
+    pagination2(i);
+}
+function pnextPage2(){
+    var i= currentPage2+1;
+    if(i>totalPage2){
+        i= currentPage2;
+    }
+    pagination2(i);
+}
+function lastPage2() {
+    var i = totalPage2;
+    pagination2(i);
+}
+//分页结束
+
+
 function getImages4Task() {
     let $imageBody = $("#image4TaskTableBody");
     $imageBody.empty("tr");
