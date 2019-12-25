0d56fae21536a428c32a93887a9fc88b53de13d7
SinLapis <stradust0001@gmail.com>
2019-12-25 17:27:48 +0800

    调整配置文件模版

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     id("org.springframework.boot") version "2.2.2.RELEASE"
